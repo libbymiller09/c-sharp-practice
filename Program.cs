642535fdfb2bb82eaa127316e4746761f1c25855
Elisabeth Miller <libbymiller09@gmail.com>
2021-01-04 12:07:21 -0600

    Adds how to use String Data Types

--- Program.cs
+++ Program.cs
@@ -19,21 +19,43 @@ namespace DeclaringVariables
     //        Console.WriteLine("The value of myInt is " + myInt + " and the value of myDouble is " + myDouble + " .");
     //    }
     //}
+    //{
+    // Using Arithmetic Operators
+    //    static void Main(string[] args)
+    //    {
+    //        int myInt = 5 + 5;
+    //        Console.WriteLine(myInt);
+
+    //        double myDouble = 45.0 / 2;
+    //        Console.WriteLine(myDouble);
+
+    //        double remain = 5 % 4;
+    //        Console.WriteLine(remain);
+
+    //        int result = 2 + 3 * 4;
+    //        Console.WriteLine("The result is {0}", result);
+    //    }
+    //}
     {
-        // Using Arithmetic Operators
+        // Using String Data Types
         static void Main(string[] args)
         {
-            int myInt = 5 + 5;
-            Console.WriteLine(myInt);
+            string myString = "Hello Universe";
 
-            double myDouble = 45.0 / 2;
-            Console.WriteLine(myDouble);
+            string name1 = "Libby";
+            string name2 = "Josh";
 
-            double remain = 5 % 4;
-            Console.WriteLine(remain);
+            //int namesEqual = string.Compare(name1, name2);
+            int namesEqual = name1.CompareTo(name2);
+            Console.WriteLine("Names equal {0}", namesEqual);
 
-            int result = 2 + 3 * 4;
-            Console.WriteLine("The result is {0}", result);
+            Console.WriteLine("{0} has {1} characters", name1, name1.Length);
+
+            string firstCharOfName = name1.Substring(0, 1);
+            string middleSectionOfName = name1.Substring(1, 3);
+
+            bool startsWith = name1.StartsWith("Li");
+
         }
     }
 }
